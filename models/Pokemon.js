4d6161ba9b036932d0648d54e6a57f9f8b7ff500
Lawrence Ardosa <lawrence@singlife.com>
2022-06-28 11:33:55 +0800

    update

--- models/Pokemon.js
+++ models/Pokemon.js
@@ -4,6 +4,19 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { len: 3 },
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    details: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
   });
 
   return Pokemon;
