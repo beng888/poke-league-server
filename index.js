0e445b72620e457b4ee9bc270402bd044f885199
Lawrence Ardosa <lawrence@singlife.com>
2022-06-28 10:45:45 +0800

    update

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ app.use(errorHandler);
 
 db.sequelize
   .sync()
-  .then(() => app.listen(process.env.PORT))
+  .then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+      console.log(`Server is running on port 4000`);
+    });
+  })
   .catch((err) => {
     console.log(err);
   });
