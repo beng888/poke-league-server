f04725ad343476cf977d886bfdb897fbfc2b44f7
Lawrence Ardosa <lawrence@singlife.com>
2022-06-29 08:40:47 +0800

    added fix

--- middlewares/errorMiddleware.js
+++ middlewares/errorMiddleware.js
@@ -4,13 +4,18 @@ const errorHandler = (err, req, res, next) => {
   const isValidationError = err.message.includes(validationError);
   const isNotNullError = err.message.includes(notNullError);
 
+  const getStatusCode = () => {
+    if (isValidationError || isNotNullError) return 500;
+    return res.statusCode ? res.statusCode : 500;
+  };
+
   const getErrorMessage = () => {
     if (isValidationError) return err.message.split(validationError)[1];
     if (isNotNullError) return err.message.split(notNullError)[1];
     return err.message;
   };
 
-  res.status(500).json({
+  res.status(getStatusCode()).json({
     message: getErrorMessage(),
     stack: process.env.NODE_ENV === 'production' ? null : err.stack,
   });
